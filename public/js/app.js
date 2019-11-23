a6c65a393e6f64bc6102269e58dff934f6e1caae
dwdhoyles <dwdhoyles@hotmail.com>
2019-11-22 23:20:48 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
     writePOne.textContent = 'Loading...'
     writePTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 writePOne.textContent = data.error
